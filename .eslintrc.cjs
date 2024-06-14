49bdac18459bc2f6cfa2e3d02a0fd9f03255e6f7
nbogie <neillbogie@googlemail.com>
2024-06-14 15:00:41 +0100

    eslint: prop-types and no-unused-vars are IN (commented out suppressors)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,10 @@ module.exports = {
     settings: { react: { version: "18.2" } },
     plugins: ["react-refresh"],
     rules: {
-        "no-unused-vars": "off",
+        // "react/prop-types": "off",//disable if not using prop-types
+        // "no-unused-vars": "off",
         "no-debugger": "off",
         "react/jsx-no-target-blank": "off",
-        "react/prop-types": "off",
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
