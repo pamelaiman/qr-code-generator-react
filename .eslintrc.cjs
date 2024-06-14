e291a8c47c79572abc6dad6f2c57df1764d78e2a
nbogie <neillbogie@googlemail.com>
2024-06-14 13:56:16 +0100

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,10 @@ module.exports = {
     settings: { react: { version: "18.2" } },
     plugins: ["react-refresh"],
     rules: {
+        "no-unused-vars": "off",
+        "no-debugger": "off",
         "react/jsx-no-target-blank": "off",
+        "react/prop-types": "off",
         "react-refresh/only-export-components": [
             "warn",
             { allowConstantExport: true },
