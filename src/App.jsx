c8d59fc8d04b53f139774eb7d5d697ecdf89a1c6
Pamela Iman <pamelariman@gmail.com>
2024-06-28 16:31:24 +0100

    template made for qr generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { DemonstratingProps } from "./components/demo/DemonstratingProps";
+import QRCode from "./QRCode";
 
-function App() {
+export default function App() {
     return (
         <div>
             <h1>App component</h1>
-            <DemonstratingProps />
+            <QRCode />
         </div>
     );
 }
-
-export default App;
---
+++ src/QRCode.jsx
@@ -0,0 +1,5 @@
+import qrcode from "qrcode";
+
+export default function QRCode() {
+    return true;
+}
